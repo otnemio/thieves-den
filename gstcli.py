7a94aef865839d5f7f6065132f5260ac24a57849
Mayank Sachan <otnemio@gmail.com>
2022-10-29 20:00:54 +0530

    wip: match case implementation

--- gstcli.py
+++ gstcli.py
@@ -8,10 +8,17 @@ def displayHelp():
     input_console.print('List of GSTNs')
     input_console.print('list')
 
+def exit():
+    input_console.print('Thanks for spending your time in this application. Help us improve so that we can save your time.')
+    quit()
+
 if __name__ == '__main__':
     p5 = lambda rsstr : int(20*float(rsstr))
     while True:
         cmd = Prompt.ask("₹")
-        argLst = cmd.split(sep=' ')
-        if argLst[0] == 'help':
-            displayHelp()
+        # argLst = cmd.split(sep=' ')
+        match cmd.split():
+            case [ 'help' ]:
+                displayHelp()
+            case [ 'exit' ]:
+                exit()
